02a05ef375c5a5b1c150ecc8a9581f7310ab27fd
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2025-01-06 21:27:57 +0900

    fix typo

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nixpkgs / NixOS version to use.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     
     home-manager = {
       url = "github:nix-community/home-manager";
