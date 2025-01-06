4cc18fbe591ad23c6d13dc743e89a961ac365b98
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2025-01-06 21:08:21 +0900

    modified the template file to use my own setup

--- flake.nix
+++ flake.nix
@@ -3,20 +3,42 @@
 
   inputs = {
     # Nixpkgs / NixOS version to use.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
-    # Helix editor, the flake.nix is presented in the github master branch
-    #helix.url = "github:helix-editor/helix/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-     # specialArgs = { inherit inputs; };
-      modules = [
-	# import the configuration.nix
-	./configuration.nix
-      ];
+  outputs = { self, home-manager, nixpkgs, ... } @ inputs: 
+  let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages =
+      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    overlays = import ./overlays {inherit inputs;};
+    nixosConfigurations = {
+      vm-nix = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [./hosts/vm-nix/nix];
+      };
+    };
+    homeConfigurations = {
+      "nixtimmy" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [./home/nixtimmy/vm-nix.nix];
+      };
     };
   };
 }
