1f5a8632d73ca40419c06880484a2a2fdb5e960d
nixtimmy <nixtimmy@timmytandian.com>
2025-01-06 22:13:16 +0900

    experiment with package installation

--- hosts/vm-nix/configuration.nix
+++ hosts/vm-nix/configuration.nix
@@ -100,7 +100,7 @@
   environment.systemPackages = with pkgs; [
     git
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #wget
+    wget
    # inputs.helix.packages."${pkgs.system}".helix
   ];
 
